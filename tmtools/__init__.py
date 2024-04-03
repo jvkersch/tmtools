7eb0f6d3f962f6febfb1ffee82477277c9f5e1d7
Cam McMenamie <c.mcmenamie@unsw.edu.au>
2024-04-03 12:59:15 +1100

    add error checking wrapper to `tm_align` binding function

--- tmtools/__init__.py
+++ tmtools/__init__.py
@@ -1,1 +1,18 @@
-from ._bindings import tm_align  # noqa
+from ._bindings import tm_align as tm_align_function  # noqa
+
+SEQ_LENGTH_MIN = 3
+
+def tm_align(*args, **kwargs):
+    """
+    Wrapper function for `tm_align` for error handling. 
+    """
+    # Check seq1 and seq2
+    coords1, coords2, seq1, seq2 = args 
+    if len(seq1) < SEQ_LENGTH_MIN or len(seq2) < SEQ_LENGTH_MIN:
+        raise ValueError(f"Sequences must be at least {SEQ_LENGTH_MIN} characters long.")
+    try:
+        return tm_align_function(*args, **kwargs)
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    
