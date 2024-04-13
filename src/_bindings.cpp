c7e4fb720761b96a89fcc764be451e7199d59ad7
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2024-04-13 14:16:48 +0900

    Add docstrings

--- src/_bindings.cpp
+++ src/_bindings.cpp
@@ -80,13 +80,42 @@ static TM_result tm_align(c_array x, c_array y, std::string seqx,
   return TM_result(t, u, TM1, TM2);
 }
 
+const char* tm_align_docstring =
+    "Perform structural aligment of proteins using the TM-score algorithm.\n"
+    "\n"
+    "Parameters\n"
+    "----------\n"
+    "x : ndarray\n"
+    "    Coordinates of the backbone of protein 1\n"
+    "y : ndarray\n"
+    "    Coordinates of the backbone of protein 2\n"
+    "seqx : str\n"
+    "    Protein sequence 1\n"
+    "seqy : str\n"
+    "    Protein sequence 2\n"
+    "\n"
+    "Returns\n"
+    "-------\n"
+    "result : TM_result\n"
+    "    An object with the optimal translation and rotation, as well as\n"
+    "    the TM-scores.\n";
+
+
 PYBIND11_MODULE(_bindings, m) {
   m.doc() = "Low-level Python wrappers for tm-align";
-  m.def("tm_align", &tm_align);
+  m.def("tm_align",
+        &tm_align,
+        tm_align_docstring,
+        py::arg("x"),
+        py::arg("y"),
+        py::arg("seqx"),
+        py::arg("seqy"));
 
-  py::class_<TM_result>(m, "TMResult")
-      .def_readonly("t", &TM_result::t)
-      .def_readonly("u", &TM_result::u)
-      .def_readonly("tm_norm_chain2", &TM_result::TM1)
-      .def_readonly("tm_norm_chain1", &TM_result::TM2);
+  py::class_<TM_result>(m, "TMResult", "Results wrapper for the TM-align algorithm")
+      .def_readonly("t", &TM_result::t, "Optimal translation from protein 1 to 2")
+      .def_readonly("u", &TM_result::u, "Optimal rotation from protein 1 to 2")
+      .def_readonly("tm_norm_chain2", &TM_result::TM1,
+                    "TM-score normalized by the length of protein 2")
+      .def_readonly("tm_norm_chain1", &TM_result::TM2,
+                    "TM-score normalized by the length of protein 1");
 }
