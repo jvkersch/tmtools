08a910d3773bf1bc76e65d00fd93adceb23ad730
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-07-12 15:03:14 -0400

    refactor(helpers): rename variables for clarity

--- tmtools/helpers.py
+++ tmtools/helpers.py
@@ -3,20 +3,22 @@ import numpy as np
 
 def apply_tmalign_transformation(structure, tmalign_result):
     # Create a copy of the structure to transform
-    transformed_structure = structure.copy()
-    transformed_structure.id = transformed_structure.id + "_tmalign"
+    aligned_structure = original_structure.copy()
+    aligned_structure.id = aligned_structure.id + "_tmalign"
 
     # Create rotation matrix and translation vector as numpy arrays
-    rotation = np.array(tmalign_result.u, dtype=float)
-    translation = np.array(tmalign_result.t, dtype=float)
+    rotation_matrix = np.array(tmalign_result.u, dtype=float)
+    translation_vector = np.array(tmalign_result.t, dtype=float)
 
     # Iterate through all atoms in the structure applying transformation
-    for model in transformed_structure:
+    for model in aligned_structure:
         for chain in model:
             for residue in chain:
                 for atom in residue:
-                    old_coord = atom.coord
-                    new_coord = np.dot(rotation, old_coord) + translation
-                    atom.set_coord(new_coord)
+                    original_coord = atom.coord
+                    transformed_coord = (
+                        np.dot(rotation_matrix, original_coord) + translation_vector
+                    )
+                    atom.set_coord(transformed_coord)
 
-    return transformed_structure
+    return aligned_structure
