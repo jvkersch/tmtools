afd757c92d9f5a9d7e964724c6824641f7b34004
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-07-13 23:29:23 -0400

    test(bindings): RMSD test for equal structures
    Closes #19

--- tmtools/tests/test_bindings.py
+++ tmtools/tests/test_bindings.py
@@ -27,6 +27,7 @@ class TestBindings(unittest.TestCase):
         # Then
         nptest.assert_array_almost_equal(res.t, np.zeros(3))
         nptest.assert_array_almost_equal(res.u, np.eye(3))
+        nptest.assert_almost_equal(res.rmsd, 0.0)
 
     def test_call_different(self):
         # Given
